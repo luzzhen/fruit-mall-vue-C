6e16ad0891a82f69a1be1d5e80fcd90c217b1a4f
luzzhen <76541574+luzzhen@users.noreply.github.com>
2021-04-12 10:53:07 +0800

    初始化水果商城项目开发环境

---
+++ fruit-mall-vue-C/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <van-button type="default">默认按钮</van-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="less">
+*{
+  margin: 0;
+  padding: 0;
+}
+#app {
+  width: 750px;
+  height :100px;
+  // background-color: black;
+}
+</style>
